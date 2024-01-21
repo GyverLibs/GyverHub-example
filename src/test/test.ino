48b3a9dcc4223fb4ccce508c2b4d5b0b0dcf4c4c
GyverLibs <alexgyvershow@ya.ru>
2024-01-21 12:12:27 +0300

    upd

--- src/test/test.ino
+++ src/test/test.ino
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <GyverHub.h>
+GyverHub hub;
+
+void build(gh::Builder& b) {
+    bool sw;
+    if (b.Switch(&sw).click()) digitalWrite(LED_BUILTIN, !sw);
+}
 
 void setup() {
-    
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("My Hub");
+
+    hub.config(F("MyDevices"), F("LED"));
+    hub.onBuild(build);
+    hub.begin();
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, 1);
 }
 
 void loop() {
-    
+    hub.tick();
 }
